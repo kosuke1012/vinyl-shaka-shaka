cb46586be349106aa7068c3a6d43c5a5d46a0ed3
Kosuke Yamagishi <kosuke.yamagishi@smartdrive.co.jp>
2021-12-06 01:14:41 +0900

    より小さく素早く動かすように

--- vinyl-shaka-shaka.ino
+++ vinyl-shaka-shaka.ino
@@ -21,15 +21,15 @@ void setup()
 
 void loop()
 {
-  for (pos = 30; pos <= 150; pos += 1)
+  for (pos = 60; pos <= 120; pos += 1)
   { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    delay(4);           // waits 15ms for the servo to reach the position
+    delay(2);           // waits 15ms for the servo to reach the position
   }
-  for (pos = 150; pos >= 30; pos -= 1)
+  for (pos = 120; pos >= 60; pos -= 1)
   {                     // goes from 180 degrees to 0 degrees
     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    delay(4);           // waits 15ms for the servo to reach the position
+    delay(2);           // waits 15ms for the servo to reach the position
   }
 }
